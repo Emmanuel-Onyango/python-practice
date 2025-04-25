4063c28970f470b221cc7f952a272cfb6339ec99
Emmanuel Onyango <emmanuelonyango238@gmail.com>
2025-04-25 20:32:49 +0300

    Add README

---
+++ sum elements in a list.py
@@ -0,0 +1,17 @@
+#number_list=[1,2,3]
+#total=0
+#for x in number_list:
+ #   total+=x
+#print(total)
+
+
+def sum_func():
+    total = 0
+    for x in number_list:
+        total+=x
+    print(f"For list{number_list}:\n Sum=",total)
+
+
+
+number_list=[1,2,3,4,]
+sum_func()
