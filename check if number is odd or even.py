36df76ff7d5a53f9bad3da0cdb20893c12801768
Emmanuel Onyango <emmanuelonyango238@gmail.com>
2025-04-26 09:24:23 +0300

    Add the check the parity, eveness or oddness, of a number

---
+++ check if number is odd or even.py
@@ -0,0 +1,9 @@
+
+#parity()
+
+def parity(x):#Parity refers to the evenness or oddness of a number
+    if x%2==0:
+        print("Even Number")#if a number is divisible by 2, then it is an even parity
+    else:
+        print("Odd Number")#if not, it is in odd parity
+parity(12)#The number to is passed as an arguments to the function and it returns it's parity
