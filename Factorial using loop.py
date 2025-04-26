6119b2cee5bc113c345e9e05975f469d5067102b
Emmanuel Onyango <emmanuelonyango238@gmail.com>
2025-04-26 19:23:54 +0300

    edited my comments

--- Factorial using loop.py
+++ Factorial using loop.py
@@ -1,8 +1,8 @@
 
 def loop_Factorial(n):
-    store=1 #initialized as 1 to store the (number-1)factorial part
-    for x in range((n), 0, -1):# The range in combination with the sore variable ensures that
-                               #The 0 instance is well handled since x can never be zero, the last possible output is 1,
+    store=1 #initialized as 1 to handle the (number-1)factorial part
+    for x in range((n), 0, -1):# The range in combination with the store variable ensures that
+                               #the 0 instance is well handled since x can never be zero, the last possible output is 1,
                                # because 0 factorial is 1
                                #The logic of the range starts from the number whose factorial is to be found
         store *=x          # product is stored after each iteration
