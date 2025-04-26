e1172a14d1f3c041a7bc7c0070fd2baee182199c
Emmanuel Onyango <emmanuelonyango238@gmail.com>
2025-04-26 16:35:08 +0300

    Add Sum of digits of a number.py file

---
+++ Sum of digits of a number.py
@@ -0,0 +1,10 @@
+
+
+def sum_Digits(digit):
+    sum = 0  # initialized the sum variable to zero and store the sum of the digits
+    for x in str(digit):#for loop to iterate each digit type casted to a string for the loop
+        sum = int(sum) + int(x)# addition of one digit after the other following each iteration and stored
+                               # in the sum variable as an integer
+    return sum
+print(sum_Digits(2))
+
